0c0acb1462d8d3446c6b5e34e5feceb1329d3e85
Zhao  Weiyu <zhaowe32@ug238.eecg.toronto.edu>
2019-10-14 13:41:26 -0400

    initial working tage and MPKI collection script

---
+++ run_test.sh
@@ -0,0 +1,12 @@
+res_raw="bm_results_raw"
+res="bm_results"
+
+mv $res_raw {$res_raw}_old
+for i in $(ls ../benchmarks/*.gz)
+do
+    echo $i >> $res_raw
+    predictor $i >> $res_raw
+done;
+
+mv $res {$res}_old
+grep "openend: MISPRED_PER_1K_INST" $res_raw | grep -oP "[0-9]+\.[0-9]+" > $res
